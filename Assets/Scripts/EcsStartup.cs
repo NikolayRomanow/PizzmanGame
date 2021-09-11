5a14c247d7da35c332dfeb1957f321c708dda52b
Nikita Romanov <system.program.code@gmail.com>
2021-09-11 10:37:39 +0300

    Added sprites to project

--- Assets/Scripts/EcsStartup.cs
+++ Assets/Scripts/EcsStartup.cs
@@ -1,44 +1,50 @@
 using Leopotam.Ecs;
 using UnityEngine;
 
-namespace Client {
-    sealed class EcsStartup : MonoBehaviour {
+namespace Client
+{
+    sealed class EcsStartup : MonoBehaviour
+    {
         EcsWorld _world;
         EcsSystems _systems;
 
-        void Start () {
+        void Start()
+        {
             // void can be switched to IEnumerator for support coroutines.
-            
-            _world = new EcsWorld ();
-            _systems = new EcsSystems (_world);
+
+            _world = new EcsWorld();
+            _systems = new EcsSystems(_world);
 #if UNITY_EDITOR
-            Leopotam.Ecs.UnityIntegration.EcsWorldObserver.Create (_world);
-            Leopotam.Ecs.UnityIntegration.EcsSystemsObserver.Create (_systems);
+            Leopotam.Ecs.UnityIntegration.EcsWorldObserver.Create(_world);
+            Leopotam.Ecs.UnityIntegration.EcsSystemsObserver.Create(_systems);
 #endif
             _systems
                 // register your systems here, for example:
                 // .Add (new TestSystem1 ())
                 // .Add (new TestSystem2 ())
-                
+
                 // register one-frame components (order is important), for example:
                 // .OneFrame<TestComponent1> ()
                 // .OneFrame<TestComponent2> ()
-                
+
                 // inject service instances here (order doesn't important), for example:
                 // .Inject (new CameraService ())
                 // .Inject (new NavMeshSupport ())
-                .Init ();
+                .Init();
         }
 
-        void Update () {
-            _systems?.Run ();
+        void Update()
+        {
+            _systems?.Run();
         }
 
-        void OnDestroy () {
-            if (_systems != null) {
-                _systems.Destroy ();
+        void OnDestroy()
+        {
+            if (_systems != null)
+            {
+                _systems.Destroy();
                 _systems = null;
-                _world.Destroy ();
+                _world.Destroy();
                 _world = null;
             }
         }
